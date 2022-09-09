9f7925e1b7675346e12d0ab13b9b0e6b95250dd6
thecountofdalocristo <84456508+thecountofdalocristo@users.noreply.github.com>
2022-09-09 11:44:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-
-
-var paragraph = document.getElementByClass("main-element-1");
-var text = document.createTextNode("This just got added");
-
-paragraph.appendChild(text);
-
-
 function juicyTuesday() {
   console.log("oh, behave!")
 }
